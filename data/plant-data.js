d196ea61ec06ea344bd461e6ad3a8746badd759f
Joseph-K-B <303josephb@gmail.com>
2021-07-25 11:43:02 -0700

    created initial data structure, adding descriptions

---
+++ data/plant-data.js
@@ -0,0 +1,119 @@
+const tomato = {
+    id: tomato,
+    name: 'tomato',
+    image: 'x',
+    icon: 'c',
+    description: `Herbaceous annual in the Solanaceae latin family. 
+    Grown for edible fruit. The plant produces yellow flowers. 
+    These flowers develop into a round fruit that can come in a variety of colors. `,
+    family: 'nightshade',
+    space: 'z',
+    recomendations: 'a',
+    companions: 'marigold, oregano, parsley, onions/chives, basil, amaranth'
+};
+const cucumber = {
+    id: cucumber,
+    name: 'cucumber',
+    image: 'x',
+    icon: 'c',
+    description: `Warm season, vining annual. Part of the Cucurbitaceae latin family.
+    Produces yellow flowers that typically 
+     into curved cylinder fruit although shapes may vary.`,
+    family: 'gourd',
+    space: 'z',
+    recomendations: 'a',
+    companions: 'b'
+};
+const pepper = {
+    id: pepper,
+    name: 'pepper',
+    image: 'x',
+    icon: 'c',
+    description: `Peppers come in a variety of shapes, sizes, and colors. 
+    They are part of the Capisicum genus that is a sub-family of the Solanaceae latin family,
+    commonly reffered to as nightshades. The vast variety of peppers encompasses a large scope of flavors.
+    Peppers are perennials meaning they will grow back for several years given the proper care`,
+    family: 'nightshade',
+    space: 'z',
+    recomendations: 'a',
+    companions: 'b'
+};
+const corn = {
+    id: corn,
+    name: 'corn',
+    image: 'x',
+    icon: 'c',
+    description: `Corn is an annual grass in the latin Paceae family. 
+    It is vital to the global food system. The plant can grow up to 100cm tall 
+    and is believed to have originated from Central America`,
+    family: 'grass',
+    space: 'z',
+    recomendations: 'a',
+    companions: 'b'
+};
+const berries = {
+    id: berries,
+    name: 'berries',
+    image: 'x',
+    icon: 'c',
+    description: 'y',
+    family: 'heather',
+    space: 'z',
+    recomendations: 'a',
+    companions: 'b'
+};
+const carrot = {
+    id: carrot,
+    name: 'carrot',
+    image: 'x',
+    icon: 'c',
+    description: 'y',
+    family: 'apiaceae',
+    space: 'z',
+    recomendations: 'a',
+    companions: 'b'
+};
+const potato = {
+    id: potato,
+    name: 'potato',
+    image: 'x',
+    icon: 'c',
+    description: 'y',
+    family: 'solanaceae',
+    space: 'z',
+    recomendations: 'a',
+    companions: 'b'
+};
+const radish = {
+    id: radish,
+    name: 'radish',
+    image: 'x',
+    icon: 'c',
+    description: 'y',
+    family: 'brassicaceae',
+    space: 'z',
+    recomendations: 'a',
+    companions: 'b'
+};
+const squash = {
+    id: squash,
+    name: 'squash',
+    image: 'x',
+    icon: 'c',
+    description: 'y',
+    family: 'cucurbita',
+    space: 'z',
+    recomendations: 'a',
+    companions: 'b'
+};
+const lettuce = {
+    id: lettuce,
+    name: 'lettuce',
+    image: 'x',
+    icon: 'c',
+    description: 'y',
+    family: 'daisy',
+    space: 'z',
+    recomendations: 'a',
+    companions: 'b'
+};
