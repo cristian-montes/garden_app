d60adec9b0b10090cac0375699b7d5be56caa94e
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-07-26 11:38:11 -0700

    added data to plant and companion data. added assests folder icons and two images in icon and assets.

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 // import functions
+import { saveUser, getUser, findByUser } from './data/data-functions.js';
+import { createUser } from './user/create-user.js';
 
+// reference needed DOM elements
+
+const userForm = document.getElementById('user-form');
+const logInBtn = document.getElementById('login-btn');
+// const 
+// const createBtn = document.getElementById('create-btn');
+
+// set event listeners 
+
+// userForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     const formData = new FormData(userForm);
+//     const user = getUser(formData);
+//     // add function find by item to look for existing users
+//     // window.location.replace = './list';
+// });
+
+userForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(userForm);
+    const newUser = createUser(formData);
+    saveUser(newUser);
+    window.location.replace('./list');
+});
+
+logInBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const formData = new FormData(userForm);
+    const oldUser = getUser();
+    findByUser(formData);
+    if (oldUser.password === formData.get('password')) {
+        window.location.replace('./choice-page');
+        return alert('Welcome Back');
+    } else if (oldUser.password === !oldUser.password){
+        // window.location.reload();
+    } else {
+        return alert('Wrong Password');
+    }
+});
 // reference needed DOM elements
 
 
---
+++ data/companion-data.js
@@ -0,0 +1,14 @@
+const marigold = {
+    id: 'marigold',
+    name: 'Marigold',
+    image: 'marigold.jpeg',
+    icon: 'marigold-icon.jpg',
+    purpose: `The flower is a natural deterrent to pests and plagues.`,
+    space: 1,
+};
+
+const companions = [
+    marigold
+];
+
+export default companions;
