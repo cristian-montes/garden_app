d3d77ec573a64dbf66a558fc23a5a2eb6d467c38
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-07-27 16:29:56 -0700

    added changes to render companion to pull from user choice.

--- companion/companion.js
+++ companion/companion.js
@@ -6,13 +6,15 @@ import { getUser, saveUser, findById, findByUser } from '../data/data-functions.
 const userSection = document.getElementById('user-section');
 const companionSection = document.getElementById('companion-section');
 
+const user = getUser();
+let friends = user.plant;
 
 for (let item of plants) {
     const showIcons = renderIcons(item);
     userSection.appendChild(showIcons);
 }
 
-for (let item of companionPlants) {
+for (let item of friends) {
     const showCompanions = renderCompanion(item);
     companionSection.appendChild(showCompanions);
 }
