227b05b98c266e6a13978413c1e1f6aad7360179
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-07-30 08:55:47 -0700

    changed the chart data and hide the second group of data.

--- map-page/map.js
+++ map-page/map.js
@@ -16,6 +16,7 @@ const compaSection = document.getElementById('compa-section');
 
 const user = getUser();
 let name = [];
+let companionName = [];
 let plantChart = [];
 let companionChart = [];
 let veggies = user.plant;
@@ -34,7 +35,7 @@ for (let item of veggies){
 // RENDER PICKED COMPANIONS
 for (let item of companions){
     const dataCompanions = findById(companionPlants, item.id);
-    name.push(item.name);
+    companionName.push(item.name);
     companionChart.push(item.qty);
     const showCompanions = renderCompanionRow(dataCompanions, item.qty);
 
@@ -59,6 +60,46 @@ new Chart(ctx, {
             ],
             borderWidth: 3 },
         { label: 'companions',
+            hidden: true,
+            data: companionChart,
+            backgroundColor: [
+                'brown',
+            ],
+            borderColor: [
+                'white',
+                
+            ],
+            borderWidth: 3
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var ctx1 = document.getElementById('myChart1').getContext('2d');
+// eslint-disable-next-line no-undef
+new Chart(ctx1, {
+    type: 'pie',
+    data: {
+        labels: companionName,
+        datasets: [{
+            label: 'companions',
+            data: companionChart,
+            backgroundColor: [
+                'yellow',
+            ],
+            borderColor: [
+                'white',
+               
+            ],
+            borderWidth: 3 },
+        { label: 'companions',
+            hidden: true,
             data: companionChart,
             backgroundColor: [
                 'brown',
