996550ca1145501c3c9dc52576c1342783aa2a47
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-07-30 08:39:25 -0700

    added styling to map.css.

--- map-page/map.js
+++ map-page/map.js
@@ -44,7 +44,7 @@ for (let item of companions){
 var ctx = document.getElementById('myChart').getContext('2d');
 // eslint-disable-next-line no-undef
 new Chart(ctx, {
-    type: 'bar',
+    type: 'pie',
     data: {
         labels: name,
         datasets: [{
